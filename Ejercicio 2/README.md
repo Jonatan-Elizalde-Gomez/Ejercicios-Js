fc7fa60a36f4a82f53c80ea7d060dd57dbd3cd55
Jonatan Elizalde Gomez <jonatanelizaldegomez@gmail.com>
2022-07-31 17:12:40 -0500

    Se terminó el ejercicio 2

---
+++ Ejercicio 2/README.md
@@ -0,0 +1,7 @@
+# Instrucciones
+
+La practica consiste en realizar validaciones de los campos mediante javascript. Los campos a validar son:
+
+* Nombre
+* Correo
+* Materia adeudada
--- Ejercicio 2/script.js
+++ Ejercicio 2/script.js
@@ -1,12 +1,74 @@
-const name = document.getElementById("forms__name");
-const mail = document.getElementById("forms__mail");
-const subject = document.getElementById("forms__subject");
-const button = document.getElementById("forms__submit");
+const dName = document.getElementById("forms__name");
+const dEmail = document.getElementById("forms__mail");
+const dSubject = document.getElementById("forms__subject");
+const dButton = document.getElementById("forms__submit");
+const form = document.getElementById("form");
+
+var isName = false; 
+var isEmail = false; 
+var isSubject = false; 
+
+const symbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+const numbers = /[1234567890]/;
+
+dButton.addEventListener("click", validateName);
+dButton.addEventListener("click", validateMail);
+dButton.addEventListener("click", validateSubjetct);
+dButton.addEventListener("click", sendData);
 
 
-button.addEventListener("click",validar);
-function validar(){
-    alert(name);
-    console.log(name);
 
+function validateName(){
+    let name = dName.value;
+
+    if(name.length == 0){
+        alert("No deje el nombre vacio");
+    }
+    else if(symbols.test(name)){
+        alert("No se permiten numeros ni simbolos en el nombre");
+    } 
+    else if(numbers.test(name)){
+        alert("No se permiten numeros ni simbolos en el nombre");
+    } 
+    else{
+        isName = true;
+    }
+}
+
+function validateMail(){
+    let email = dEmail.value;
+
+    if(email.length == 0){
+        alert("No deje el e-mail vacio");
+    }
+    else if(email.includes(".") && email.includes("@")){
+        isEmail = true; 
+    }
+    else{
+        alert("El e-mail no es valido")
+    }
+}
+
+function validateSubjetct(){
+    let subject = dSubject.value;
+
+    if(subject.length == 0){
+        alert("No deje la materia vacio");
+    }
+    else if(symbols.test(subject)){
+        alert("No se permiten numeros ni simbolos en la materia");
+    } 
+    else if(numbers.test(subject)){
+        alert("No se permiten numeros ni simbolos en la materia");
+    } 
+    else{
+        isSubject = true;
+    }
+}
+
+function sendData(){
+    if(isEmail && isName && isSubject){
+        alert("Todos los datos estan correctos")
+        form.submit();
+    }
 }
